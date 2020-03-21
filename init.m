c6b098582ce9531e81babe1e0481cb22f2cec6de
J Ueng <44347316+j-ueng@users.noreply.github.com>
2020-03-21 19:06:19 +0000

    Add files via upload

---
+++ init.m
@@ -0,0 +1,18 @@
+% Please run this script under the root folder
+
+clearvars; %-except N;
+close all;
+clear all;
+
+% addpaths
+addpath('./internal');
+addpath('./external');
+addpath('./external/libsvm-3.18/matlab');
+
+% initialise external libraries
+run('external/vlfeat-0.9.21/toolbox/vl_setup.m'); % vlfeat library
+cd('external/libsvm-3.18/matlab'); % libsvm library
+run('make');
+cd('../../..');
+
+% tested on Ubuntu 12.04, 64-bit, Intel® Core�?i7-3820 CPU @ 3.60GHz × 8 
